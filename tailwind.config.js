79632205560a26947ac74bcd2681aad34983cd55
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-02-20 17:46:17 +0700

    Config : Tailwindcss theme primary color

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#2E282A",
+    },
+    // extend: {},
+  },
+  plugins: [],
+};
