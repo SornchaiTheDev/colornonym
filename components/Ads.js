757514259a669a33975cc8ad3981826df2396fc9
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-02-23 22:18:59 +0700

    Fix : Ads Size

--- components/Ads.js
+++ components/Ads.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Ads() {
   return (
-    <div className="w-full h-16  bg-white text-center flex justify-center items-center">
+    <div className="w-full   bg-white text-center flex justify-center items-center">
       <img src="https://via.placeholder.com/300x50" className="w-full" />
     </div>
   );
