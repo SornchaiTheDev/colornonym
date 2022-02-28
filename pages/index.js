380e67cd85bad483bb3465af2a772183e82f7984
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-02-28 22:50:13 +0700

    Update : adsense Script tag

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,11 @@ function index() {
         <ContextProvider>
           <Head>
             <title>Colornonym</title>
-            <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5805738859470835" />
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5805738859470835"
+              crossOrigin="anonymous"
+            ></script>
           </Head>
 
           <div className="flex flex-col justify-start items-center bg-primary min-h-screen ">
@@ -23,7 +27,6 @@ function index() {
       </AuthContextProvider>
     </FirebaseContextProvider>
   );
-  
 }
 
 export default index;
