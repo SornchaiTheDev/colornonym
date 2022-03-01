2d3efba97281bc0f82c7127eeebb1cd27cbdb487
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-03-01 17:22:49 +0700

    Modified : ads section

--- components/Ads.js
+++ components/Ads.js
@@ -12,19 +12,19 @@ function Ads() {
   }, []);
   return (
     <div className="w-full relative h-16 bg-white text-center">
-      <script
+      {/* <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5805738859470835"
         crossOrigin="anonymous"
-      ></script>
-      <ins
+      ></script> */}
+      {/* <ins
         className="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-5805738859470835"
         data-ad-slot="6921238207"
         data-ad-format="auto"
         data-full-width-responsive="true"
-      ></ins>
+      ></ins> */}
     </div>
   );
 }
